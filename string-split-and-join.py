a6372b2ffe0e47ac479b78ec4b5819ca35f2984e
OliyadM <101395529+OliyadM@users.noreply.github.com>
2024-04-23 13:42:13 +0300

    Update string-split-and-join.py

--- string-split-and-join.py
+++ string-split-and-join.py
@@ -1,1 +1,11 @@
-
+def split_and_join(line):
+    a=line.split(" ")
+    result=""
+    for index in range(len(a)-1):
+        result+=a[index]+"-"
+    result+=a[-1]
+    return result
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
