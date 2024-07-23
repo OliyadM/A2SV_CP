b5da98540dc09172448fb913af89ee3a1606ca58
OliyadM <101395529+OliyadM@users.noreply.github.com>
2024-07-23 14:19:07 +0300

    Create pancake-sorting.py

---
+++ pancake-sorting.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pancakeSort(self, arr):
+        result = []
+        n = len(arr)
+        
+        for i in range(n, 1, -1):
+            max_idx = arr.index(max(arr[:i]))
+            
+            if max_idx != i - 1:
+                if max_idx != 0:
+                    arr[:max_idx + 1] = arr[:max_idx + 1][::-1]
+                    result.append(max_idx + 1)
+                
+                arr[:i] = arr[:i][::-1]
+                result.append(i)
+        
+        return result
+
