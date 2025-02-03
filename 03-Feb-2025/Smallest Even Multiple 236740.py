7aa9d617e09d4e3acead71da48a7dc28b3f27e68
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-03 17:38:46 +0300

    Created 'Smallest Even Multiple' by A2SV Hub

---
+++ 03-Feb-2025/Smallest Even Multiple 236740.py
@@ -0,0 +1,8 @@
+# Problem: Smallest Even Multiple - https://leetcode.com/problems/smallest-even-multiple/
+
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        if n%2==0:
+            return n
+        else:
+            return n*2
