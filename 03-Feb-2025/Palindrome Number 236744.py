b2c8edbe33104833f116b03040fa53acef469de3
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-03 17:41:41 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 03-Feb-2025/Palindrome Number 236744.py
@@ -0,0 +1,8 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        str_int=str(x)
+        
+        return str_int ==str_int[::-1]   
+        
