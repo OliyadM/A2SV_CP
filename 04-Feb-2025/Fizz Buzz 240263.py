25eb5c17b9b92a6bfbda9c297b9c797335a86586
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-04 17:02:40 +0300

    Created 'Fizz Buzz' by A2SV Hub

---
+++ 04-Feb-2025/Fizz Buzz 240263.py
@@ -0,0 +1,15 @@
+# Problem: Fizz Buzz - https://leetcode.com/problems/fizz-buzz/description/
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        result=[]
+        for num in range(1,n+1):
+            if num%3==0 and num%5==0:
+                result.append("FizzBuzz")
+            elif num%3==0:
+                result.append("Fizz")
+            elif num%5==0:
+                result.append("Buzz")
+            else:
+                result.append(str(num))
+        return result
