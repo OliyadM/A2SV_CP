24b10c72878641de4602c377b24f9b9c2bc7490b
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-13 15:20:21 +0300

    Created 'Segment with Small Sum' by A2SV Hub

---
+++ 13-Feb-2025/Segment with Small Sum 262691.py
@@ -0,0 +1,18 @@
+# Problem: Segment with Small Sum - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+
+def longet(arr,k):
+    n=len(arr)
+    left=0
+    cursum=0
+    long=0
+    for right in range(n):
+        cursum+=arr[right]
+        while cursum>k:
+            cursum-=arr[left]
+            left+=1
+        long=max(long,right-left+1)
+    return long
+
+n,k =map(int,input().split())
+arr=list(map(int,input().split()))
+print(longet(arr,k))
