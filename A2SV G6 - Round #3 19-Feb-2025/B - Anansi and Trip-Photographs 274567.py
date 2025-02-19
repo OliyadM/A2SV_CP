6c1a752ef2cb093b68beca2c3ccc607b0262e162
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-19 18:02:25 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Feb-2025/B - Anansi and Trip-Photographs 274567.py
@@ -0,0 +1,19 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+def photo(arr,n,x):
+    arr.sort()
+    row_1=arr[:n]
+    row_2=arr[n:]
+    for i in range(n):
+        if row_2[i] - row_1[i] <x:
+            return False
+    return True
+
+t=int(input())
+for _ in range(t):
+    n,x=map(int,input().split())
+    arr=list(map(int,input().split()))
+    if photo(arr,n,x):
+        print("YES")
+    else:
+        print("NO")
