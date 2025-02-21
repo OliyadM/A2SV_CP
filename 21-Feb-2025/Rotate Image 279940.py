b2df1edb79964d10e4175ec21ec197b4bc970dac
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-21 23:17:57 +0300

    Created 'Rotate Image' by A2SV Hub

---
+++ 21-Feb-2025/Rotate Image 279940.py
@@ -0,0 +1,15 @@
+# Problem: Rotate Image - https://leetcode.com/problems/rotate-image/
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row.reverse()
