21b5790aa431915e379161366b581bef34f3aee8
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-21 23:57:58 +0300

    Created 'Find Duplicate File in System' by A2SV Hub

---
+++ 21-Feb-2025/Find Duplicate File in System 280000.py
@@ -0,0 +1,19 @@
+# Problem: Find Duplicate File in System - https://leetcode.com/problems/find-duplicate-file-in-system/
+
+from collections import defaultdict
+
+def find_duplicate_files(paths):
+    content_to_paths = defaultdict(list)
+    
+    for path in paths:
+        parts = path.split(' ')
+        dir_path = parts[0]
+        
+        for file in parts[1:]:
+            file_name, file_content = file.split('(')
+            file_content = file_content[:-1]
+            full_file_path = dir_path + "/" + file_name
+            
+            content_to_paths[file_content].append(full_file_path)
+    
+    return [file_paths for file_paths in content_to_paths.values() if len(file_paths) > 1]
