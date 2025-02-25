a9dfa5bf111d95efa2b359a13d99e0e2e6cd6c16
OliyadM <101395529+OliyadM@users.noreply.github.com>
2025-02-25 09:51:35 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 25-Feb-2025/Fox And Snake 282226.py
@@ -0,0 +1,10 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+m,n = map(int,input().split())
+for i in range(1,m+1):
+    if i%2:
+        print('#'*n)
+    elif not i%2 and i%4:
+        print(('.'*(n-1))+'#')
+    else:
+        print('#'+('.'*(n-1)))
